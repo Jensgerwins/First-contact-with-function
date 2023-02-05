a0293c13007bfe4edf55fec86aaf1e4408c8cf2d
Jens gerwins <Jens.gerwins@gmail.com>
2023-02-05 20:38:36 +0100

    first Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function  welcomeMsg(a) {
+
+console.log("Welcome " + a);
+
+}
+welcomeMsg("Jens!");
+welcomeMsg("Marc!");
+
+function calcCrossPrice(a, b) {
+return a * b + a;
+
+}
+console.log(calcCrossPrice(20, 0.19));
+console.log(calcCrossPrice(40, 0.16));
+
+function addPositive(a , b) {
+    return a + b ;
+}
+console.log(addPositive(2, 3));
+console.log(addPositive(3, -5));
+console.log(addPositive(-1, -8));
